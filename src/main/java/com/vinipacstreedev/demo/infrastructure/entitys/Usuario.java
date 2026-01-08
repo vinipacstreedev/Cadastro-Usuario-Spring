f4fbfc274be23b586e814ed701e9a5ed68fae513
Vinicius Pereira <viniciuss.ssouzaa@gmail.com>
2026-01-08 15:44:29 -0300

    Primeiro commit

---
+++ src/main/java/com/vinipacstreedev/demo/infrastructure/entitys/Usuario.java
@@ -0,0 +1,27 @@
+package com.vinipacstreedev.demo.infrastructure.entitys;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@Table(name = "usuario")
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer Id;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "nome")
+    private String nome;
+
+}
