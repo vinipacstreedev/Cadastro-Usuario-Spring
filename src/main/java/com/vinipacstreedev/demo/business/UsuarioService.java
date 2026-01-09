cb79215020c83e38e50c6a36a54bd694ea6a79d1
Vinicius Pereira <viniciuss.ssouzaa@gmail.com>
2026-01-09 16:07:46 -0300

    parte 2

--- src/main/java/com/vinipacstreedev/demo/business/UsuarioService.java
+++ src/main/java/com/vinipacstreedev/demo/business/UsuarioService.java
@@ -2,6 +2,7 @@ package com.vinipacstreedev.demo.business;
 
 import com.vinipacstreedev.demo.infrastructure.entitys.Usuario;
 import com.vinipacstreedev.demo.infrastructure.repository.UsuarioRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,6 +23,22 @@ public class UsuarioService {
         return repository.findByEmail(email).orElseThrow(
                 () -> new RuntimeException("Email não encontrado")
         );
+    }
+
+    public void deletarUsuarioPorEmail(String email){
+        repository.deleteByEmail(email);
+    }
+
+    public void atualizarUsuarioPorId(Integer id, Usuario usuario){
+        Usuario usuarioEntity = repository.findById(id).orElseThrow(() ->
+                new RuntimeException("Usuário não encontrado"));
+        Usuario usuarioAtualizado = Usuario.builder()
+                .email(usuario.getEmail() != null ? usuario.getEmail() : usuarioEntity.getEmail() )
+                .nome(usuario.getNome() != null ? usuario.getNome() : usuarioEntity.getNome())
+                .id(usuarioEntity.getId())
+                .build();
+
+        repository.saveAndFlush(usuarioAtualizado);
     }
 
 }
--- src/main/java/com/vinipacstreedev/demo/infrastructure/repository/UsuarioRepository.java
+++ src/main/java/com/vinipacstreedev/demo/infrastructure/repository/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package com.vinipacstreedev.demo.infrastructure.repository;
 
 import com.vinipacstreedev.demo.infrastructure.entitys.Usuario;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +9,7 @@ import java.util.Optional;
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
     Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
 }
